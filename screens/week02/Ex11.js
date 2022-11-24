c4e4596279cf587bf262cc1b9a0bfccc4394e833
manlikasee <100658593+manlikasee@users.noreply.github.com>
2022-11-24 12:34:39 +0700

    week2

---
+++ screens/week02/Ex11.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Ex11() {
+  return (
+    <View style={{ flex : 1,flexDirection : 'column' }}>    
+        <View style={{
+             flex : 2,
+             backgroundColor : '#4A90E2', }}></View>   
+        <View style={{
+             flex : 2,
+             backgroundColor : '#50E3C2', }}></View>  
+   
+    </View>
+  );
+}
